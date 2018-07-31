f077f4b00638bed775799038da060d28757d8ab9
Samuel pires <samuel.pires.pro@gmail.com>
2018-07-31 11:56:25 +0100

    added empty actions in controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,16 @@
 class TasksController < ApplicationController
+  def index
+  end
+  def show
+  end
+  def new
+  end
+  def create
+  end
+  def edit
+  end
+  def update
+  end
+  def delete
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # List taks
+  get 'tasks' => 'tasks#index'
+
+  # View details of a task
+  get 'tasks/:id' => 'tasks#show'
+
+  # add a task ( new & create )
+  get 'tasks/new' => 'tasks#new'
+  post 'tasks' => 'tasks#create'
+
+  # update a task (edit & update)
+  get 'tasks/:id/edit' => 'tasks#edit'
+  patch 'tasks/:id' => 'tasks#update'
+
+  #delete a task
+  delete 'tasks/:id' => 'tasks#destroy'
+
 end
